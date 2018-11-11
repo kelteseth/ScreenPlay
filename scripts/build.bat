114aebf8b5bd5356d5ccdd5396c7fd3a11b23be5
kelteseth <i@kelteseth.net>
2018-11-11 15:51:28 +0100

    Add qmake_all

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,6 @@
 setlocal EnableExtensions
 
 set PATH=%PATH%;C:\Qt\Tools\QtCreator\bin
-set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\bin\Hostx64\x64
 set PATH=%PATH%;C:\Qt\5.11.2\msvc2017_64\bin
 set root=%cd%
 
@@ -12,6 +11,9 @@ cd package
 
 echo "Begin build of ScreenPlay "
 qmake.exe ../ScreenPlay.pro -spec win32-msvc "CONFIG+=release"
+jom.exe clean
+jom.exe qmake_all
 jom.exe  -j8
+jom.exe  install
 echo "Build ScreenPlay finished!"
 
