61aa1d58676fbe0b789668b3f978008c68e8c379
Elias Steurer <i@kelteseth.net>
2020-04-20 16:18:29 +0200

    Add MacOS version of vcpkg dependency installation

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,4 +8,9 @@ git checkout origin/master
 chmod +x bootstrap-vcpkg.sh
 ./bootstrap-vcpkg.sh
 chmod +x vcpkg
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+./vcpkg install libzippp:x64-osx nlohmann-json:x64-osx openssl-unix:x64-osx libzip:x64-osx
+else
 ./vcpkg install libzippp:x64-linux nlohmann-json:x64-linux openssl-unix:x64-linux  	libzip:x64-linux
+fi
