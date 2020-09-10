d850a05aa880d3c1d264cada21fdd87e73c4fb61
Elias Steurer <i@kelteseth.net>
2020-09-10 17:32:24 +0200

    Add ffmpeg to vcpkg dependencies

--- install_dependencies_windows.bat
+++ install_dependencies_windows.bat
@@ -4,6 +4,7 @@ cd Common
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 git pull
-git checkout 2bc6cd714
+rem  master 10.09.2020 - 18ab4b72a26284f0df28295ce7bf9b21c96f20f4
+git checkout  18ab4b72a26284f0df28295ce7bf9b21c96f20f4
 call bootstrap-vcpkg.bat
-vcpkg.exe install zlib libzip libzippp openssl breakpad  --triplet x64-windows --recurse
+vcpkg.exe install zlib libzip libzippp openssl breakpad ffmpeg[gpl,x264,vpx,mp3lame,opus]  --triplet x64-windows --recurse
