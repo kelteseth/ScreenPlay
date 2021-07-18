c8ade19ce455b6aad5ea7bd51ba3d5e4d4a64112
poly000 <1348292515@qq.com>
2021-07-18 20:07:31 +0800

    update translations

--- ScreenPlay/translations/ScreenPlay_en.ts
+++ ScreenPlay/translations/ScreenPlay_en.ts
@@ -965,11 +965,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/Settings/Settings.qml" line="99"/>
-        <source>Help us make ScreenPlay faster and more stable. All collected data is purely anonymous and only used for development purposes!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../qml/Settings/Settings.qml" line="110"/>
         <source>Set save location</source>
         <translation type="unfinished"></translation>
@@ -1007,6 +1002,11 @@
     <message>
         <location filename="../qml/Settings/Settings.qml" line="73"/>
         <source>ScreenPlay will start with Windows and will setup your Desktop every time for you.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Settings/Settings.qml" line="99"/>
+        <source>Help us make ScreenPlay faster and more stable. All collected data is purely anonymous and only used for development purposes! We use &lt;a href=&quot;https://sentry.io&quot;&gt;sentry.io&lt;/a&gt; to collect and analyze this data. A &lt;b&gt;big thanks to them&lt;/b&gt; for providing us with free premium support for open source projects!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -1369,6 +1369,123 @@
     <message>
         <location filename="../qml/Common/Dialogs/SteamNotAvailable.qml" line="11"/>
         <source>Could not load steam integration!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SteamProfile</name>
+    <message>
+        <location filename="../qml/Workshop/SteamProfile.qml" line="78"/>
+        <location filename="../qml/Workshop/SteamProfile.qml" line="138"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamProfile.qml" line="160"/>
+        <source>Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SteamWorkshop</name>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="151"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="163"/>
+        <source>Download now!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="168"/>
+        <source>Downloading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="176"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="202"/>
+        <source>Open In Steam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="286"/>
+        <source>Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="305"/>
+        <source>Search for Wallpaper and Widgets...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="349"/>
+        <source>Open Workshop in Steam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="373"/>
+        <source>Ranked By Vote</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="376"/>
+        <source>Publication Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="379"/>
+        <source>Ranked By Trend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="382"/>
+        <source>Favorited By Friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="385"/>
+        <source>Created By Friends</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="388"/>
+        <source>Created By Followed Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="391"/>
+        <source>Not Yet Rated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="394"/>
+        <source>Total VotesAsc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="397"/>
+        <source>Votes Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="400"/>
+        <source>Total Unique Subscriptions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="449"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Workshop/SteamWorkshop.qml" line="473"/>
+        <source>Forward</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -2120,109 +2237,6 @@
     <message>
         <location filename="../qml/Create/Wizards/WizardPage.qml" line="101"/>
         <source>Saving...</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>Workshop</name>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="171"/>
-        <source>Loading</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="183"/>
-        <source>Download now!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="188"/>
-        <source>Downloading...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="196"/>
-        <source>Details</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="222"/>
-        <source>Open In Steam</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="281"/>
-        <source>Search for Wallpaper and Widgets...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="347"/>
-        <source>Open Workshop in Steam</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="359"/>
-        <source>Open GameHub in Steam</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="383"/>
-        <source>Ranked By Vote</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="386"/>
-        <source>Publication Date</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="389"/>
-        <source>Ranked By Trend</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="392"/>
-        <source>Favorited By Friends</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="395"/>
-        <source>Created By Friends</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="398"/>
-        <source>Created By Followed Users</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="401"/>
-        <source>Not Yet Rated</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="404"/>
-        <source>Total VotesAsc</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="407"/>
-        <source>Votes Up</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="410"/>
-        <source>Total Unique Subscriptions</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="460"/>
-        <source>Back</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/Workshop/Workshop.qml" line="484"/>
-        <source>Forward</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
