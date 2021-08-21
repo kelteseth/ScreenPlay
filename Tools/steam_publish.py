ddcb83b79b256b635eeffdd6c6f681da6ea0888d
Elias Steurer <i@kelteseth.net>
2021-08-21 12:53:19 +0200

    Add steam publish python script with dynamic description based on git branch, hash and date

---
+++ Tools/steam_publish.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import subprocess
+import shutil
+import argparse
+from sys import platform
+from execute_util import execute
+from datetime import datetime
+import subprocess
+
+
+# Executes steamcmd with username and password. Changes the content of the config
+# for better readability in the steam builds tab
+# https://partner.steamgames.com/apps/builds/672870
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+
+
+def get_git_branch_hash():
+    return subprocess.check_output(['git', 'branch', '--show-current'])
+
+
+# MAIN
+parser = argparse.ArgumentParser(description='Publish ScreenPlay to Steam')
+parser.add_argument('-u', action="store", dest="steam_username", required=True,
+                    help="Steam Username.")
+parser.add_argument('-p', action="store", dest="steam_password", required=True,
+                    help="Steam Password.")
+parser.add_argument('-c', action="store", dest="vdf_config_name", required=True,
+                    help="Name to the .vdf config. This will use the .vdf files from the steamcmd folder.")
+args = parser.parse_args()
+
+abs_vdf_path = os.path.abspath("steamcmd/"+args.vdf_config_name)
+
+if not os.path.isfile(abs_vdf_path):
+    print("Incorrect vdf name")
+    sys.exit(-1)
+
+file = open(abs_vdf_path, "r")
+
+config_content = file.read()
+git_hash = get_git_revision_short_hash().decode("utf-8").replace("\n", "")
+git_branch = get_git_branch_hash().decode("utf-8").replace("\n", "")
+current_date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+build_description = "- git hash: " + git_hash + ", branch: " +  git_branch + " - upload datetime: " + current_date_time
+config_content = config_content.replace("{{BUILD_DESCRIPTION}}", build_description)
+tmp_steam_config_foldername = "tmp_steam_config/"
+tmp_steam_config_dir = os.path.abspath(tmp_steam_config_foldername)
+
+if os.path.isdir(tmp_steam_config_dir):
+    shutil.rmtree(tmp_steam_config_dir)
+
+os.mkdir(tmp_steam_config_dir)
+
+f = open(os.path.abspath(tmp_steam_config_dir + "/" + args.vdf_config_name), "w")
+f.write(config_content)
+f.close()
+
+tmp_steam_config_path = "\"" + os.path.abspath(tmp_steam_config_foldername + args.vdf_config_name) +  "\"" 
+
+print("Execute steamcmd on: " + tmp_steam_config_path)
+execute("steamcmd +login {username} {password} +run_app_build {config} +quit".format(username=steam_username, password=steam_password, config=tmp_steam_config_path))
+
+print("Deleting tmp config")
+shutil.rmtree(tmp_steam_config_dir)
