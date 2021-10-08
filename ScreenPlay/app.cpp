805d78c6597932e6f7f290173c2fa17b6b675081
Elias Steurer <i@kelteseth.net>
2021-10-08 10:33:11 +0200

    Bump version to 0.15

--- ScreenPlay/app.cpp
+++ ScreenPlay/app.cpp
@@ -58,7 +58,7 @@ App::App()
     QGuiApplication::setOrganizationName("ScreenPlay");
     QGuiApplication::setOrganizationDomain("screen-play.app");
     QGuiApplication::setApplicationName("ScreenPlay");
-    QGuiApplication::setApplicationVersion("0.14.0");
+    QGuiApplication::setApplicationVersion("0.15.0");
     QGuiApplication::setQuitOnLastWindowClosed(false);
 
     QFontDatabase::addApplicationFont(":/assets/fonts/LibreBaskerville-Italic.ttf");
