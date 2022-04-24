d722e6232f82e8e2c49eb3e20a19729c3fb588cb
Elias Steurer <i@kelteseth.net>
2022-04-24 09:09:56 +0200

    Add FetchContentThirdParty workaround for QtCreator

--- CMake/FetchContentThirdParty.cmake
+++ CMake/FetchContentThirdParty.cmake
@@ -2,10 +2,14 @@ cmake_minimum_required(VERSION 3.16.0)
 
 include(FetchContent)
 
-FetchContent_Declare(
+set(THIRD_PARTY_PATH "${CMAKE_SOURCE_DIR}/ThirdParty/")
+
+FetchContent_Populate(
   qml-plausible
   GIT_REPOSITORY https://gitlab.com/kelteseth/qml-plausible.git
   GIT_TAG        00398446c7a2882a11d34c007a1ed8205c72e123
+  # Workaround because:
+  # 1. QtCreator cannot handle QML_ELEMENT stuff when it is in bin folder
+  # https://bugreports.qt.io/browse/QTCREATORBUG-27083
+  SOURCE_DIR ${THIRD_PARTY_PATH}/qml-plausible
 )
-
-FetchContent_MakeAvailable(qml-plausible)
