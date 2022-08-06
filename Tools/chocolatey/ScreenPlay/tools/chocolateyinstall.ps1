81944e71483a5f011fbed82ee6dbc6efdec59cba
Elias Steurer <i@kelteseth.net>
2022-08-06 14:24:00 +0200

    Add basic chocolatey support

---
+++ Tools/chocolatey/ScreenPlay/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://kelteseth.com/screenplay/ScreenPlay-0.15.0-RC1-x64-windows-release.zip' 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+  fileType      = 'zip'
+
+  softwareName  = 'ScreenPlay*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
