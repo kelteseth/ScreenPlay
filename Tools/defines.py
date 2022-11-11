4f9fa3e266df6e825a250d0ac964b0993c04be76
Elias Steurer <i@kelteseth.net>
2022-11-11 12:19:13 +0100

    Bump IFW and vcpkg

--- Tools/defines.py
+++ Tools/defines.py
@@ -4,9 +4,9 @@ from sys import stdout
 
 stdout.reconfigure(encoding='utf-8')
 
-VCPKG_VERSION = "c72cefb"  # Master 27.10.2022
+VCPKG_VERSION = "2871ddd"  # Master 11.11.2022
 QT_VERSION = "6.3.2"
-QT_IFW_VERSION = "4.4"
+QT_IFW_VERSION = "4.5"
 SCREENPLAY_VERSION = "0.15.0-RC3"
 AQT_PATH = Path("C:/aqt") if sys.platform == "win32" else Path().home().joinpath("aqt")
 PYTHON_EXECUTABLE = "python" if sys.platform == "win32" else "python3"
