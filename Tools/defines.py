4da1f5cd045e1b386203056ca4cd287dd4dd6d3b
Elias Steurer <i@kelteseth.net>
2022-11-19 16:32:37 +0100

    Bump qt version to 6.4.1

--- Tools/defines.py
+++ Tools/defines.py
@@ -5,7 +5,7 @@ from sys import stdout
 stdout.reconfigure(encoding='utf-8')
 
 VCPKG_VERSION = "2871ddd"  # Master 11.11.2022
-QT_VERSION = "6.3.2"
+QT_VERSION = "6.4.1"
 QT_IFW_VERSION = "4.5"
 SCREENPLAY_VERSION = "0.15.0-RC3"
 AQT_PATH = Path("C:/aqt") if sys.platform == "win32" else Path().home().joinpath("aqt")
