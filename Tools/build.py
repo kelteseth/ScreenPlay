31005092beaa0eef452ab050eb68b0fc480ac922
Elias Steurer <i@kelteseth.net>
2023-01-29 13:26:36 +0100

    Bump version to 0.15.0-RC5

--- Tools/build.py
+++ Tools/build.py
@@ -37,9 +37,9 @@ class BuildResult:
     installer: Path
     # [...]/build-x64-windows-release/ScreenPlay-Installer.zip
     installer_zip: Path
-    # [...]/build-x64-windows-release/ScreenPlay-0.15.0-RC1-x64-windows-release.zip
+    # [...]/build-x64-windows-release/ScreenPlay-0.X.0-RCX-x64-windows-release.zip
     build_zip: Path
-    # [...]/build-x64-windows-release/ScreenPlay-0.15.0-RC1-x64-windows-release.txt :sha256, needed for scoop
+    # [...]/build-x64-windows-release/ScreenPlay-0.X.0-RCX-x64-windows-release.txt :sha256, needed for scoop
     build_hash: Path
     # x64, arm64, universal
     build_arch: str
--- Tools/defines.py
+++ Tools/defines.py
@@ -13,20 +13,16 @@ if sys.platform == "win32":
 elif sys.platform == "darwin":
     OS = "mac"
     QT_PLATFORM = "macos"
-elif sys.platform ==  "linux":
+elif sys.platform == "linux":
     OS = "linux"
     QT_PLATFORM = "gcc_64"
 
-SCREENPLAY_VERSION = "0.15.0-RC4"
-
-
+SCREENPLAY_VERSION = "0.15.0-RC5"
 QT_PATH = Path.cwd().parent.parent.joinpath("aqt")
 QT_VERSION = "6.5.0"
 QT_BIN_PATH = QT_PATH.joinpath(f"{QT_VERSION}/{QT_PLATFORM}/bin")
 QT_TOOLS_PATH = QT_PATH.joinpath("Tools/")
 QT_IFW_VERSION = "4.5"
-
 VCPKG_VERSION = "2871ddd"  # Master 11.11.2022
-
 PYTHON_EXECUTABLE = "python" if sys.platform == "win32" else "python3"
 FFMPEG_VERSION = "5.0.1"
