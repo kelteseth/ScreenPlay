f60e44bbda97732538db5a2d62d3b3940abebacc
Elias Steurer <i@kelteseth.net>
2025-05-26 17:02:45 +0200

    Fix version check to work with both casings, thank you @WenjunHuang !

--- CMake/GetProjectVersion.cmake
+++ CMake/GetProjectVersion.cmake
@@ -22,7 +22,7 @@ function(get_project_version VERSION_VAR)
         )
 
         message(STATUS "Parsing git tag: ${GIT_VERSION}")
-        string(REPLACE "v" "" STRIPPED_VERSION "${GIT_VERSION}") # Remove the 'v' prefix
+        string(REGEX REPLACE "^[vV]" "" STRIPPED_VERSION "${GIT_VERSION}") # Remove the 'v/V' prefix
         string(REPLACE "-" ";" VERSION_LIST ${STRIPPED_VERSION})
         list(GET VERSION_LIST 0 VERSION_STRING)
 
