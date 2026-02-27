6c0daf96669c3accee45be431fb9410f97f6f82b
Elias Steurer <i@kelteseth.net>
2026-02-27 16:32:55 +0100

    Update vpckg, qcoro and windows ffmpeg

--- Tools/defines.py
+++ Tools/defines.py
@@ -41,8 +41,8 @@ QT6_SOURCE_MODULES = [
 ]
 QT_TOOLS_PATH = QT_PATH.joinpath("Tools/")
 QT_IFW_VERSION = "4.8.1"
-# 04.09.2025 https://github.com/microsoft/vcpkg :
-VCPKG_VERSION = "2dc54df"
+# 27.02.2026 https://github.com/microsoft/vcpkg :
+VCPKG_VERSION = "ffc071e"
 VCPKG_BASE_PACKAGES =  [
         "curl",
         "openssl",
@@ -54,7 +54,7 @@ VCPKG_BASE_PACKAGES =  [
     ]
 PYTHON_EXECUTABLE = "python" if sys.platform == "win32" else "python3"
 FFMPEG_VERSION_MAC = "8.0"
-FFMPEG_VERSION_WIN = "8.0.0"
+FFMPEG_VERSION_WIN = "8.0.1"
 # None stable version omit patch: 4.3-rc2 or 4.5 stable
 GODOT_VERSION = "4.5.1"
 GODOT_RELEASE_TYPE = "stable"
